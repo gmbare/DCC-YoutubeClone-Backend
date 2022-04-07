31775c3052347c72bd5e292a51c33c2c011402b5
Garett Bare <Garett.Bare@yahoo.com>
2022-04-06 19:37:46 -0500

    Successfull connection to MongoDB

---
+++ Routes/comments.js
@@ -0,0 +1,8 @@
+// const {Comment, validateComment} = require('../models/comment');
+const express = require("express");
+const { Router } = require('express');
+const router = express.Router();
+
+//ENDPOINTS GO HERE// 
+
+module.exports = router
